d7584a70cde4e2c636f984edb1aa045e0ca22407
Kwon_Gwan <44615788+Kwonkuhwan@users.noreply.github.com>
2022-11-24 18:05:07 +0900

    2022.11.24 - 테스트용 캐릭터 교체

---
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    private PlayerInputActions actions = null;
+    private Rigidbody2D rigid2D = null;
+    private Animator anim = null;
+    private SpriteRenderer[] spriteRenderers = null;
+    private SpriteRenderer spriteRenderer = null;
+
+    private Vector2 inputVec = Vector3.zero;
+
+    public GameObject[] objs = null;
+    public float speed = 3.0f;
+
+    #region Unity �Լ�
+
+    private void Awake()
+    {
+        actions = new();
+        rigid2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    { 
+    }
+
+    private void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        if (inputVec.sqrMagnitude > 0.0f)
+        {
+            Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+            rigid2D.MovePosition(rigid2D.position + nextVec);
+            anim.SetBool("Move", true);
+        }
+        else
+        {
+            anim.SetBool("Move", false);
+        }
+        
+    }
+
+    private void OnEnable()
+    {
+        actions.Player.Enable();
+        actions.Player.Move.performed += OnPlayerMove;
+        actions.Player.Move.canceled += OnPlayerMove;
+    }
+
+    private void OnDisable()
+    {
+        actions.Player.Move.canceled -= OnPlayerMove;
+        actions.Player.Move.performed -= OnPlayerMove;
+        actions.Player.Disable();
+    }
+
+    #endregion
+
+    #region Actions �Լ�
+    private void OnPlayerMove(InputAction.CallbackContext context)
+    {
+        inputVec = context.ReadValue<Vector2>();
+
+        if(inputVec.x < 0.0f)
+        {
+            foreach(var sr in spriteRenderers)
+            {
+                sr.flipX = true;
+            }
+        }
+        else if(inputVec.x > 0.0f)
+        {
+            foreach (var sr in spriteRenderers)
+            {
+                sr.flipX = false;
+            }
+        }
+    }
+    #endregion
+}
